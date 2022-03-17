458f57c703471279746c6956e72fb0504743888e
Santhosh <santhosh.bs.kumar@gmail.com>
2022-03-17 08:26:00 +0800

    kata 1: add, contains & height

--- src/main/java/com/santhosh/algorithms/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/santhosh/algorithms/datastructures/binarysearchtree/BinarySearchTree.java
@@ -2,97 +2,18 @@ package com.santhosh.algorithms.datastructures.binarysearchtree;
 
 //https://github.com/williamfiset/Algorithms/blob/master/src/test/java/com/williamfiset/algorithms/datastructures/binarysearchtree/BinarySearchTreeTest.java
 public class BinarySearchTree<T extends Comparable<T>> {
-    // Track number of nodes in this BST
-    private int nodeCount = 0;
-    private Node root;
-
     // Add an element to this binary tree.
     // Return true if insertion is successful
     public boolean add(T element) {
-        if(contains(element)) {
-            return false;
-            // otherwise, add element in binary tree
-        } else {
-            root = add(root, element);
-            nodeCount++;
-            return true;
-        }
-    }
-
-    // recursively add a value in binary tree
-    private Node add(Node node, T element) {
-
-        //Base Case: found a leaf node
-        if (node == null) {
-            node = new Node(element, null, null);
-            return node;
-        } else {
-            // Pick a subtree to insert element
-            if (element.compareTo(node.data) < 0) {
-                node.left = add(node.left, element);
-                return node;
-            } else {
-                node.right = add(node.right, element);
-                return node;
-            }
-        }
-    }
-
-    // returns true if the element contains in the tree
-    private boolean contains(T element) {
-        return contains(root, element);
-    }
-
-    // recursive method to find element in the tree
-    private boolean contains(Node node, T element) {
-
-        // Base case: Reached bottom value not found
-        if (node == null) {
-            return false;
-        }
-
-        // compare element with the current value
-        int cmp = element.compareTo(node.data);
-
-        // value we are looking for is less than the current node's value
-        // so look in left subtree
-        if (cmp < 0) {
-            return contains(node.left, element);
-        } else if (cmp > 0) { // value we are looking for is greater than the current node's value
-            return contains(node.right, element);
-        } else { // we have found the value
-            return true;
-        }
+        return false;
     }
 
     // Computes the height of the tree
     public int height() {
-        return height(root);
-    }
-
-    // Recursive helper method to compute height of the tree
-    private int height(Node node) {
-        if (node == null) {
-            return 0;
-        }
-        int height = Math.max(height(node.left), height(node.right)) + 1;
-        return height;
-    }
-
-
-    // It contains actual data & references of left & right node
-    private class Node {
-        T data;
-        Node left, right;
-
-        public Node(T data, Node left, Node right) {
-            this.data = data;
-            this.left = left;
-            this.right = right;
-        }
+        return 0;
     }
 
     public boolean isEmpty() {
-        return nodeCount == 0;
+        return false;
     }
 }
